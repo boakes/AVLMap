57b195aa43201230c65ca587465b8761bc0d82a6
boakes <boakes@users.noreply.github.com>
2015-10-25 23:30:02 -0500

    10/25/15 23:30:02

---
+++ AVLMap.cpp
@@ -0,0 +1,200 @@
+#include "AVLMap.h"
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <chrono>
+using std::map;
+using std::make_pair;
+using std::cout;
+using std::vector;
+using std::random_shuffle;
+using std::pair;
+
+void testone() {
+	AVLMap<int,int> test; 
+	pair<int,int> fst;
+	fst = make_pair(1,1); 
+	pair<int,int> snd;
+	snd = make_pair(2,2);
+	pair<int,int> thrd;
+	thrd = make_pair(3,3);
+	auto itrfrst = test.insert(fst);
+	auto itrfrstfail = test.insert(fst);
+	cout << test[1] << "\n";
+	test.insert(snd);
+	cout << test[2] << "\n";
+	test.insert(thrd);
+	cout << test[3] << "\n";
+	cout << test.count(1) << "\n";
+	cout << test.count(0) << "\n";
+	cout << "This should be a 1: " << (itrfrst.first == itrfrstfail.first);
+	cout << "\nThis should be a 0: " << (itrfrst.second == itrfrstfail.second);
+	test.erase(2);
+	cout << "\n"<< test.count(2) << "\n";
+	for(auto x:test){
+		cout << "\n" << "Ranged loop";
+		cout << x.first << "\n";
+	}
+
+	test.clear();
+
+}
+
+
+
+bool equalitytest(){
+	int test_size = 1000;
+	bool testbool = true;
+	AVLMap<int,int> test;
+	map<int,int> testmap;
+	vector<int> RandomThing;
+	for(int i = 0; i<test_size; ++i){
+		RandomThing.push_back(i);
+	}
+    random_shuffle(RandomThing.begin(), RandomThing.end());
+
+    for(int i=0;i<test_size;++i){
+    	test.insert(make_pair(RandomThing[i],RandomThing[i]));
+    	testmap.insert(make_pair(RandomThing[i],RandomThing[i]));
+    }
+
+    auto ths = test.begin()++++++++++++;
+    auto thsafter = ths;
+    thsafter++;
+    auto ers = test.erase(ths);
+    if(ers != thsafter){
+    	testbool = false;
+    }
+	for(auto bgn = test.begin();bgn != test.end(); ++bgn){
+		if((*bgn).second != testmap[(*bgn).first]){
+			testbool = false;
+		}
+	}
+	return testbool;
+}
+
+
+void chartest() {
+	AVLMap<int,char> test; 
+	pair<int,char> fst;
+	fst = make_pair(1,'c'); 
+	pair<int,char> snd;
+	snd = make_pair(2,'d');
+	pair<int,char> thrd;
+	thrd = make_pair(3,'e');
+	auto itrfrst = test.insert(fst);
+	auto itrfrstfail = test.insert(fst);
+	cout << test[1] << "\n";
+	test.insert(snd);
+	cout << test[2] << "\n";
+	test.insert(thrd);
+	cout << test[3] << "\n";
+	cout << test.count(1) << "\n";
+	cout << test.count(0) << "\n";
+	cout << "This should be a 1: " << (itrfrst.first == itrfrstfail.first);
+	cout << "\nThis should be a 0: " << (itrfrst.second == itrfrstfail.second);
+	test.erase(2);
+	cout << "\n"<< test.count(2) << "\n";
+	for(auto x:test){
+		cout << "\n" << "Ranged loop";
+		cout << x.first << "\n";
+	}
+}
+
+
+bool runTests(){
+	int test_size = 2000000;
+    AVLMap<int, int> firstbst;
+	map<int,int> firstmap;
+	
+	vector<int> randoms;
+
+	for(int i = 0; i<test_size; ++i){
+		randoms.push_back(i);
+	}
+	//cout << randoms[0] << "\n";
+    random_shuffle(randoms.begin(), randoms.end());
+	//cout << randoms[0] << "\n";
+
+	auto t1a = std::chrono::high_resolution_clock::now();
+	for(int i=0; i<test_size; ++i){
+	    firstbst.insert(make_pair(randoms[i],randoms[i]));
+		firstmap.insert(make_pair(randoms[i],randoms[i]));
+	}
+	auto t2a = std::chrono::high_resolution_clock::now();
+
+   std::cout << "inserting " << test_size << " took "
+           	  << std::chrono::duration_cast<std::chrono::milliseconds>(t2a-t1a).count()///1000
+              << " milliseconds\n";
+
+	random_shuffle(randoms.begin(), randoms.end()--);
+	for(int i=0; i<test_size; ++i){
+		if(firstbst[randoms[i]]!=firstmap[randoms[i]]){
+			cout << "check with map failed\n";
+			return false;
+		}
+	}	
+	
+	cout << "Copyconstructor\n";
+	AVLMap<int, int> tsbtsrif(firstbst);
+	cout << "Copy constructor finished \n";
+	
+	const auto a = tsbtsrif;
+	const auto b = firstbst;
+
+	if(a != b){
+		cout << "copy construct worked\n";
+		return false;
+	}
+
+	firstbst[2000000];
+	auto x = firstbst.find(2000000);
+	auto y = firstbst.insert(make_pair(2000000,1));
+	if(x == y.first && ((*x).second) != 1){
+		cout << "SUCCESS!\n";
+	}
+	/*
+	firstbst.clear();
+	random_shuffle(randoms.begin(), randoms.end());
+	//cout << randoms[0] << "\n";
+	for(int i=0; i<test_size; ++i){
+	    firstbst.insert(make_pair(randoms[i],randoms[i]));
+		//firstmap.insert(make_pair(randoms[i],randoms[i]));
+	}
+
+	firstbst.clear();
+	random_shuffle(randoms.begin(), randoms.end());
+	//cout << randoms[0] << "\n";
+	for(int i=0; i<test_size; ++i){
+	    firstbst.insert(make_pair(randoms[i],randoms[i]));
+		//firstmap.insert(make_pair(randoms[i],randoms[i]));
+	}
+	*/
+
+	return true;	
+}
+
+int main(){
+	testone();
+	cout << "\n---------Testone passed---------\n";
+	auto t1 = std::chrono::high_resolution_clock::now();
+    runTests();
+	auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout << "test function took "
+           	  << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count()///1000
+              << " milliseconds\n";
+/*	
+
+	cout << "\n---------RunTests passed---------\n";
+	if(equalitytest()) {cout << "\n---------Equality Test Passed---------\n";}
+	else cout << "\n---------Equality Test Failed---------\n";
+	chartest();
+	cout << "\n---------CharTests ran---------\n";
+	return 0; 	
+
+*/
+}
+
+
---
+++ AVLMap.h
@@ -0,0 +1,409 @@
+#ifndef AVLMap
+#define AVLMap 
+
+#include <iostream> 
+#include <stdlib.h>
+template<typename K,typename V>
+class AVLMap {
+    
+private:
+    struct Node{
+        Node* right;
+        Node* left; 
+        Node* parent; 
+        std::pair<K,V> nodepr; 
+        int ht; 
+
+        Node(Node* r, Node* l, Node* p, K k, V v, int h){
+            right = r;
+            left = l;
+            parent = p; 
+            nodepr = std::make_pair(k,v);
+            ht = h;
+        }
+    };
+    int sz; 
+    Node* root;
+    
+public:
+
+    typedef K key_type;
+    typedef V mapped_type;
+    typedef std::pair<K,V> value_type;
+
+    class const_iterator;
+
+    class iterator {
+        Node* loc; 
+        bool itrend;
+    public:
+        friend class const_iterator;
+        iterator(Node* l,bool b):loc(l),itrend(b){}
+
+        Node* Successor(Node* x){
+         if (x->right != nullptr){
+            return AVLMap<K,V>::minNode(x->right);
+         } 
+          Node* y = x->parent;
+          while (y != nullptr && x == y->right){
+                x = y;
+                y = y->parent;
+            }
+            return y;
+        }
+
+        Node* Predecessor(Node* x){
+         if (x->left != nullptr){
+            return AVLMap<K,V>::maxNode(x->left);
+         } 
+         Node* y = x->parent;
+         while (y != nullptr && x == y->left){
+                x = y;
+                y = y->parent;
+         }
+         return y;
+        }
+
+        bool operator==(const iterator &i) const { 
+            return (loc == i.loc && itrend == i.itrend); 
+        }
+
+        bool operator!=(const iterator &i) const { return !(*this==i); }
+        std::pair<K,V> &operator*() { return loc -> nodepr; }
+       
+        iterator &operator++() {
+            Node* tmp = Successor(loc);
+            if(tmp == nullptr){
+                itrend = true;
+            }else{
+                loc = tmp;
+                itrend =false;
+            }
+            return *this;
+        }
+        iterator &operator--() {
+            if(itrend){
+                itrend = false;
+                return *this;
+            }else{
+                loc = Predecessor(loc);
+                return *this;
+            }
+        }
+        iterator operator++(int) {
+            iterator tmp(*this);
+            ++(*this);
+            return tmp;
+        }
+        iterator operator--(int) {
+            iterator tmp(*this);
+            --(*this);
+            return tmp;
+        }
+    };
+
+    class const_iterator {
+        Node* loc; 
+        bool itrend;
+    public:
+        friend class AVLMap<K,V>;  // You might not need this in your code, but it helped me.
+        const_iterator(Node* l,bool b):loc(l),itrend(b) {}
+        // TODO: Other constructors as needed.
+        const_iterator(const iterator &iter):loc(iter.loc),itrend(iter.itrend){}
+
+        Node* Successor(Node* x){
+         if (x->right != nullptr){
+            return AVLMap<K,V>::minNode(x->right);
+         } 
+          Node* y = x->parent;
+          while (y != nullptr && x == y->right){
+                x = y;
+                y = y->parent;
+            }
+            return y;
+        }
+
+        Node* Predecessor(Node* x){
+         if (x->left != nullptr){
+            return AVLMap<K,V>::maxNode(x->left);
+         } 
+         Node* y = x->parent;
+         while (y != nullptr && x == y->left){
+                x = y;
+                y = y->parent;
+            }
+            return y;
+        }
+
+        bool operator==(const const_iterator &i) const { return (loc == i.loc && itrend == i.itrend);  }
+        bool operator!=(const const_iterator &i) const { return !(*this==i); }
+        const std::pair<K,V> &operator*() { return loc -> nodepr; }
+        const_iterator &operator++() {
+            Node* tmp = Successor(loc);
+            if(tmp == nullptr){
+                itrend = true;
+            }else{
+                loc = tmp;
+                itrend=false;
+            }
+            return *this;
+        }
+        const_iterator &operator--() {
+            if(itrend){
+                itrend = false;
+            }else{
+                loc = Predecessor(loc);
+            }
+            return *this;
+        }
+        const_iterator operator++(int) {
+            const_iterator tmp(*this);
+            ++(*this);
+            return tmp;
+        }
+        const_iterator operator--(int) {
+            const_iterator tmp(*this);
+            --(*this);
+            return tmp;
+        }
+    };
+
+    static Node* maxNode(Node* x){
+        while (x->right != nullptr){
+            x = x->right;
+        } return x;
+    }
+
+    static Node* minNode(Node* x){
+        while (x->left != nullptr){
+            x = x->left;
+        } return x;
+    }
+
+    AVLMap(){
+       root = nullptr;
+       sz = 0; 
+    }
+    ~AVLMap(){
+       clear();
+    }
+    void helpCopier(Node *x){
+        if(x!=nullptr){
+            insert(x->nodepr);
+            helpCopier(x->left);
+            helpCopier(x->right);
+        }
+    }
+
+    AVLMap(const AVLMap<K,V> &that){
+        root = nullptr; 
+        sz = 0; 
+        helpCopier(that.root);
+    }
+
+    AVLMap &operator=(const AVLMap<K,V> &that) {
+        clear();
+        root = nullptr;
+        sz = 0; 
+        helpCopier(that.root);
+        return *this;
+    }
+
+    bool empty() const {return sz == 0;}
+
+    unsigned size() const {return sz;}
+
+    Node* getroot(){
+        return root;
+    }
+
+    Node* fancy_find(Node* nd, const key_type& k) const{
+        if(nd == nullptr || k == nd->nodepr.first){
+            return nd;
+        }if(k < nd->nodepr.first){
+            return fancy_find(nd->left,k);
+        } else if (k > nd->nodepr.first){
+            return fancy_find(nd->right,k);
+        } else return nd;
+    }
+
+    iterator find(const key_type& k){
+        auto fndr = fancy_find(root,k);
+        if(fndr == nullptr){
+            return end();
+        }else {
+            return iterator(fndr,false);
+        }
+    }
+
+    const_iterator find(const key_type& k) const{
+        auto fndr = fancy_find(root,k);
+        if(fndr == nullptr){
+            return cend();
+        }else {
+            return iterator(fndr,false);
+        }
+    }
+
+    unsigned int count(const key_type& k) const{
+        auto fndr = fancy_find(root,k);
+        if(fndr == nullptr){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    std::pair<iterator,bool> insert(const value_type& val){
+        iterator tmp = find(val.first);
+        if(tmp != end()){
+            return std::make_pair(tmp,false);
+        }
+        Node* z = new Node(nullptr,nullptr,nullptr,val.first,val.second);
+        Node* y = nullptr;
+        Node* x = root;
+        ++sz;
+        while (x != nullptr){
+            y = x;
+            if(z->nodepr.first < x->nodepr.first){
+                x = x->left;
+            }else{x = x->right;}
+        }
+        z->parent = y;
+        if(y == nullptr){
+            root = z;
+        } else if(z->nodepr.first < y->nodepr.first){
+            y->left = z;
+        } else{
+            y->right = z;
+        }
+        return std::make_pair(iterator(z,false),true);
+    }
+
+    template <class InputIterator>
+    void insert(InputIterator first, InputIterator last) {
+        for(auto iter = first; iter!=last; ++iter) {
+            insert(*iter);
+        }
+    }
+
+    void transplant(Node* u, Node* v){
+        if(u->parent == nullptr){
+            root = v;
+        } else if (u == u->parent->left){
+            u->parent->left = v;
+        } else u->parent->right = v;
+        if (v != nullptr){
+            v->parent = u->parent;
+        }
+    }
+
+    unsigned int erase(const key_type& k){
+        Node* z = fancy_find(root,k);
+        if(z == nullptr){
+            return 0;
+        }else{
+         Node* tmp = z; 
+            if(z->left == nullptr){
+                transplant(z,z->right);
+            }else if(z->right == nullptr){
+                transplant(z,z->left);
+            }else { 
+                Node* y = minNode(z->right);
+                if(y->parent != z){
+                  transplant(y,y->right);
+                  y->right = z->right;
+                  y->right->parent = y;
+                }
+                transplant(z,y);
+                y->left = z->left;
+                y->left->parent = y;
+            }
+            delete tmp;
+            --sz;
+            return 1;
+        }
+    }
+
+    iterator erase(const_iterator position){
+        iterator tmp(position.loc,position.itrend);
+        ++tmp; 
+        erase((*position).first);
+        return tmp;
+    }
+
+    void cleartree(Node* x){  
+        if(x!=nullptr){
+            if(x->left!=nullptr)cleartree(x->left);
+            if(x->right!=nullptr)cleartree(x->right);
+            delete x;
+        }
+    }
+
+    void clear(){
+        if(root!= nullptr) cleartree(root);
+        sz = 0;
+        root = nullptr;
+    }
+
+    mapped_type &operator[](const K &key){
+        std::pair<K,V> jk;
+        jk.first = key;
+        auto tmp = insert(jk); 
+        return (*(tmp.first)).second;
+   }
+   
+    bool operator==(const AVLMap<K,V>& rhs) const{
+       if(sz != rhs.sz){
+        return false;
+       }
+       for(auto thsitr = begin(); thsitr != end(); ++thsitr){
+         if(rhs.count((*thsitr).first) == 0){
+            return false;
+         }
+       }
+       return true;
+    }
+
+    bool operator!=(const AVLMap<K,V>& rhs) const{
+        return !(*this == rhs);
+    }
+
+    iterator begin() {
+        if(sz == 0){
+            return end();
+        }else {
+            return iterator(minNode(root),false);
+        }
+    }
+    
+    const_iterator begin() const { return cbegin();}
+    
+    iterator end() {
+        if(sz == 0){
+            return iterator(root,true); 
+        }else {
+            return iterator(maxNode(root),true); 
+        }
+    }
+
+    const_iterator end() const { return cend(); }
+
+    const_iterator cbegin() const { 
+        if(sz == 0){
+            return cend();
+        }else {
+            return const_iterator(minNode(root),false); 
+        }
+    }
+    const_iterator cend() const { 
+        if(sz == 0){
+            return const_iterator(root,true);
+        } else {
+            return const_iterator(maxNode(root),true);
+        }
+    }
+
+};
+
+#endif
